9d9da53a67733269cbcbb8c23943111cd2adb6ee
Neha Vekaria <nehavekaria@live.com>
2019-10-31 13:10:02 +1300

    add and display feature

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -7,10 +7,6 @@ const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case VisibilityFilters.SHOW_ALL:
       return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
     default:
       throw new Error('Unknown filter: ' + filter)
   }
@@ -27,4 +23,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(TodoList)
+)(TodoList) 
